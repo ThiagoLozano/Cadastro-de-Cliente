37d63c986d1a72d86253ae9582b27ae8fb787980
ThiagoLozano <51161587+ThiagoLozano@users.noreply.github.com>
2020-07-23 18:58:10 -0300

    Adicionei as alterações do Documento.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+# Cria conexão com o BD.
 myBD = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -7,6 +8,7 @@ myBD = mysql.connector.connect(
     database="lojaPython"
 )
 
+# Cria um Cursor.
 cursor = myBD.cursor()
 cursor.execute('USE lojaPython')
 
@@ -17,56 +19,65 @@ def cadastrarCliente():
         print('{:^30}'.format('CADASTRANDO UM NOVO CLIENTE'))
         print('=' * 30)
 
+        # Primeiro Nome.
         p_nome = str(input('Primeiro Nome: ')).capitalize()
         while len(p_nome) >= 255 or len(p_nome) <= 0:
-            print('Erro: Dado Inválido!')
+            print('\033[31mErro: Dado Inválido!\033[m')
             p_nome = str(input('Primeiro Nome: ')).capitalize()
 
+        # Último Nome.
         u_nome = str(input('Último Nome: ')).capitalize()
         while len(u_nome) >= 255 or len(u_nome) <= 0:
-            print('Erro: Dado inválido!')
+            print('\033[31mErro: Dado inválido!\033[m')
             u_nome = str(input('Último Nome: ')).capitalize()
 
+        # Idade.
         while True:
             try:
                 idade = int(input('Idade: '))
                 if idade >= 100 or idade <= 0:
                     raise ValueError()
             except ValueError:
-                print('Erro: Tipo de Dado Inválido')
+                print('\033[31mErro: Tipo de Dado Inválido\033[m')
             else:
                 break
 
+        # Sexo.
         sexo = str(input('Sexo: ')).upper()
         while len(sexo) > 2 or len(sexo) <= 0 or sexo != 'M' and sexo != 'F':
-            print('Erro: Dado Inválido')
+            print('\033[31mErro: Dado Inválido\033[m')
             sexo = str(input('Sexo: ')).upper()
 
+        # Endereço.
         endereco = str(input('Endereço: ')).capitalize()
         while len(endereco) > 255 or len(endereco) <= 0:
-            print('Erro: Dado Inválido')
+            print('\033[31mErro: Dado Inválido\033[m')
             endereco = str(input('Endereço: ')).capitalize()
 
+        # Cidade
         cidade = str(input('Cidade: ')).capitalize()
         while len(cidade) > 255 or len(cidade) <= 0:
-            print('Erro: Dado Inválido')
+            print('\033[31mErro: Dado Inválido\033[m')
             cidade = str(input('Cidade: ')).capitalize()
 
+        # Estado.
         estado = str(input('Estado (Sigla): ')).upper()
         while len(estado) > 2 or len(estado) <= 0:
-            print('Erro: Dado Inválido')
+            print('\033[31mErro: Dado Inválido\033[m')
             estado = str(input('Estado (Sigla): ')).upper()
 
+        # Avaliação.
         while True:
             try:
                 avaliacao = int(input('Avaliação da Loja (0 á 5): '))
                 if avaliacao > 5 or avaliacao < 0:
                     raise ValueError()
             except ValueError:
-                print('Erro: Tipo de Dado Inválido')
+                print('\033[31mErro: Tipo de Dado Inválido\033[m')
             else:
                 break
 
+        # Insere os valores dentro da Tabela
         cursor.execute(
             "INSERT INTO Cliente VALUES (DEFAULT, '{}', '{}', {}, '{}', '{}', '{}', '{}', {})".format(p_nome, u_nome,
                                                                                                       idade, sexo,
@@ -84,13 +95,17 @@ def buscarCliente():
     busca_pNome = str(input('Primeiro Nome: '))
     busca_uNome = str(input('Último Nome: '))
 
+    # Passa pela tabela procurando os valores inseridos.
     cursor.execute("""SELECT * FROM Cliente
-                   WHERE Primeiro_Nome = '{}' AND Ultimo_Nome = '{}' """.format(busca_pNome, busca_uNome))
+                WHERE Primeiro_Nome = '{}' AND Ultimo_Nome = '{}' """.format(busca_pNome, busca_uNome))
 
     select = cursor.fetchall()
 
     for x in select:
         print(x)
+    else:
+        # Retorna uma mensagem caso não encontre nada relacionado aos valores inseridos.
+        print('\033[31m\nDesculpe! Não encontrei nada relacionado a sua busca.\033[m')
 
 
 def consultarCliente():
@@ -98,6 +113,7 @@ def consultarCliente():
     print('{:^30}'.format('CONSULTANDO TODOS OS CLIENTES'))
     print('=' * 30)
 
+    # Retorna todos os registros já inseridos na tabela.
     cursor.execute("SELECT * FROM Cliente")
 
     select = cursor.fetchall()
@@ -108,6 +124,7 @@ def consultarCliente():
     print('\n')
 
 
+# Menu.
 while True:
     print('=' * 30)
     print('{:^28}'.format('MENU'))
